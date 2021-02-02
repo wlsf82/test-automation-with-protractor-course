2ae06aa0baf018d0a5f9ce127356fcb4e5370c11
Walmyr <wlsf82@gmail.com>
2021-02-02 16:41:35 +0100

    Implement arrange and act steps of login test

--- specs/login.spec.js
+++ specs/login.spec.js
@@ -1,6 +1,14 @@
 describe('Login', function() {
   it('logs in successfully', function() {
+    browser.get('/login')
 
+    const emailField = element(by.id('email'))
+    const passwordField = element(by.id('password'))
+    const loginButton = element(by.cssContainingText('button[type="submit"]', 'Login'))
+
+    emailField.sendKeys('admin@example.com')
+    passwordField.sendKeys('Passw0rd!')
+    loginButton.click()
   })
 })
 
