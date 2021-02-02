d2d8241f1e59c278f96efe9ce74053ca4bad04b4
Walmyr <wlsf82@gmail.com>
2021-02-02 16:44:14 +0100

    Add test assertion

--- specs/login.spec.js
+++ specs/login.spec.js
@@ -5,10 +5,13 @@ describe('Login', function() {
     const emailField = element(by.id('email'))
     const passwordField = element(by.id('password'))
     const loginButton = element(by.cssContainingText('button[type="submit"]', 'Login'))
+    const yourNotesHeading = element(by.cssContainingText('h1', 'Your Notes'))
 
     emailField.sendKeys('admin@example.com')
     passwordField.sendKeys('Passw0rd!')
     loginButton.click()
+
+    expect(yourNotesHeading.isDisplayed()).toBe(true)
   })
 })
 
