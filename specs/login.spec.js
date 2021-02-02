2cb5916d131caf7e12f32b70e47b105f482aa612
Walmyr <wlsf82@gmail.com>
2021-02-02 17:09:04 +0100

    Add tests for login button verifications

--- specs/login.spec.js
+++ specs/login.spec.js
@@ -1,15 +1,42 @@
 const helper = require('protractor-helper')
 
 describe('Login', function() {
+  let emailField
+  let passwordField
+  let loginButton
+  let yourNotesHeading
+  let logoutLink
+
+  beforeEach(() => {
+    browser.get('login')
+    emailField = element(by.id('email'))
+    passwordField = element(by.id('password'))
+    loginButton = element(by.cssContainingText('button[type="submit"]', 'Login'))
+    yourNotesHeading = element(by.cssContainingText('h1', 'Your Notes'))
+    logoutLink = element(by.cssContainingText('.navbar-right a', 'Logout'))
+  })
+
+  describe('Login button verifications', () => {
+    it('starts disabled', () => {
+      helper.waitForElementVisibility(loginButton)
+
+      expect(loginButton.getAttribute('disabled')).toBeDefined()
+    })
+
+    it('is kept disabled when filling only the email field', () => {
+      helper.fillFieldWithText(emailField, 'test-email@example.com')
+
+      expect(loginButton.getAttribute('disabled')).toBeDefined()
+    })
+
+    it('is kept disabled when filling only the password field', () => {
+      helper.fillFieldWithText(passwordField, 'S3creT')
+
+      expect(loginButton.getAttribute('disabled')).toBeDefined()
+    })
+  })
+
   it('logs in and out successfully', function() {
-    browser.get('/login')
-
-    const emailField = element(by.id('email'))
-    const passwordField = element(by.id('password'))
-    const loginButton = element(by.cssContainingText('button[type="submit"]', 'Login'))
-    const yourNotesHeading = element(by.cssContainingText('h1', 'Your Notes'))
-    const logoutLink = element(by.cssContainingText('.navbar-right a', 'Logout'))
-
     helper.fillFieldWithText(emailField, process.env.USER_EMAIL)
     helper.fillFieldWithText(passwordField, process.env.USER_PASSWORD)
     helper.click(loginButton)
