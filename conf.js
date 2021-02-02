337c12377e818ac3050821e6019e1080d64fa061
Walmyr <wlsf82@gmail.com>
2021-02-02 16:31:41 +0100

    Maximize browser on preparation

--- conf.js
+++ conf.js
@@ -2,6 +2,7 @@ module.exports.config = {
   baseUrl: 'https://notes-serverless-app.com',
   specs: ['specs/*.spec.js'],
   onPrepare: () => {
+    browser.driver.manage().window().maximize()
     browser.waitForAngularEnabled(false)
   }
 }
