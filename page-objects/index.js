0be59bbdeaf205f27edbb3c9510f6154ed22f848
Walmyr <wlsf82@gmail.com>
2021-02-04 11:35:05 +0100

    Add test for crud operations

--- page-objects/index.js
+++ page-objects/index.js
@@ -4,7 +4,14 @@ const emailField = element(by.id('email'))
 const passwordField = element(by.id('password'))
 const loginButton = element(by.cssContainingText('button[type="submit"]', 'Login'))
 const yourNotesHeading = element(by.cssContainingText('h1', 'Your Notes'))
+const loginLink = element(by.cssContainingText('.navbar-right a', 'Login'))
 const logoutLink = element(by.cssContainingText('.navbar-right a', 'Logout'))
+const createNewNoteLink = element(by.css('.list-group [href="/notes/new"]'))
+const textArea = element(by.id('content'))
+const createButton = element(by.cssContainingText('button[type="submit"]', 'Create'))
+const saveButton = element(by.cssContainingText('button[type="submit"]', 'Save'))
+const deleteButton = element(by.cssContainingText('.btn-danger', 'Delete'))
+const notes = element(by.className('list-group'))
 
 function login(
   email = process.env.USER_EMAIL,
@@ -15,11 +22,42 @@ function login(
   helper.click(loginButton)
 }
 
+function createNote(note) {
+  helper.fillFieldWithText(textArea, note)
+  helper.click(createButton)
+}
+
+function openNote(note) {
+  const noteToOpen = element(by.cssContainingText('.list-group-item', note))
+  helper.click(noteToOpen)
+}
+
+function editNote(note) {
+  helper.clearFieldAndFillItWithText(textArea, note.updatedContent)
+  helper.click(saveButton)
+}
+
+function deleteNote() {
+  helper.click(deleteButton)
+  helper.waitForAlertToBePresent()
+
+  const alertDialog = browser.switchTo().alert()
+  expect(alertDialog.getText()).toEqual('Are you sure you want to delete this note?')
+  alertDialog.accept()
+}
+
 module.exports = {
   emailField,
   passwordField,
   loginButton,
   yourNotesHeading,
+  loginLink,
   logoutLink,
-  login
+  createNewNoteLink,
+  notes,
+  login,
+  createNote,
+  openNote,
+  editNote,
+  deleteNote
 }
---
+++ specs/crud.spec.js
@@ -0,0 +1,36 @@
+const faker = require('faker')
+const helper = require('protractor-helper')
+const page = require('../page-objects')
+
+describe('CRUD operations', () => {
+  beforeEach(() => {
+    browser.get('login')
+    page.login()
+    helper.click(page.createNewNoteLink)
+  })
+
+  it('successfully CRUD a note', () => {
+    const note = {
+      initialContent: faker.random.words(5),
+      updatedContent: faker.random.words(6)
+    }
+
+    page.createNote(note.initialContent)
+
+    helper.waitForTextToBePresentInElement(page.notes, note.initialContent)
+
+    page.openNote(note.initialContent)
+    page.editNote(note)
+
+    helper.waitForTextNotToBePresentInElement(page.notes, note.initialContent)
+    helper.waitForTextToBePresentInElement(page.notes, note.updatedContent)
+
+    page.openNote(note.updatedContent)
+    page.deleteNote()
+
+    helper.waitForTextNotToBePresentInElement(page.notes, note.updatedContent)
+
+    helper.click(page.logoutLink)
+    helper.waitForElementVisibility(page.loginLink)
+  })
+})
